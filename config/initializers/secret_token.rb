1b6a41cccb6bd7d336dc41bdd4d12189cfd82d2d
Roy McKenzie <roymckenzie@gmail.com>
2014-04-05 16:04:53 -0700

    Added initial gems.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bayareabikeshare::Application.config.secret_key_base = 'c77bf9820608ab0c5473e216dac60a4b2fdb2f30aa1ece181b7445c31101388c1678a979d95716b639c258575b27e593948f0014b80f6476c32173e293b6e7b1'
