cfc8af62541bacbd1c92fc4d310350dd7c93d8c5
Roy McKenzie <roymckenzie@gmail.com>
2014-04-05 20:08:45 -0700

    Added some stats to info windows.

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -6,7 +6,7 @@ class StationsController < ApplicationController
     #Get an array of coordinates for map
     @station_coordinates = Array.new
     @stations.each do |s|
-      @station_coordinates.push(:latlng => [s.lat,s.long], :popup => "#{s.name}")
+      @station_coordinates.push(:latlng => [s.lat,s.long], :popup => "<strong>#{s.name}</strong><br>#{s.dockcount} bikes<br>#{s.start_trips.count} pickups<br>#{s.end_trips.count} dropoffs")
     end
 
   end
