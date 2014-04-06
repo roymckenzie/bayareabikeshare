32b4e4bf4d6c2965ae0dee0520c5a1eaf0e2c8d6
Roy McKenzie <roymckenzie@gmail.com>
2014-04-06 01:10:42 -0700

    Added special markers to map.

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -1,13 +1,8 @@
 class StationsController < ApplicationController
+
   def index
 
     @stations = Station.all
-
-    #Get an array of coordinates for map
-    @station_coordinates = Array.new
-    @stations.each do |s|
-      @station_coordinates.push(:latlng => [s.lat,s.long], :popup => "<strong>#{s.name}</strong><br>#{s.dockcount} bikes<br>#{s.start_trips.count} pickups<br>#{s.end_trips.count} dropoffs")
-    end
 
   end
 end
