2fc8070b3e8aff4e73029c943d7085dcd52047b9
Roy McKenzie <roymckenzie@gmail.com>
2014-04-08 03:25:36 -0700

    Added heatmap functionality.

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -18,24 +18,48 @@ class StationsController < ApplicationController
     else
       @stations = Station.where(landmark: params[:city])
     end
-    @geojson = Array.new
 
+    # Most popular drop off station for city
     popular_dropoff = @stations.find(@trips.where({ end_station_id: @stations.uniq.pluck(:id)}).group('end_station_id').order('count_end_station_id DESC').count('end_station_id').first[0])
+
+    # Most popular pickup station for city
     popular_pickup = @stations.find(@trips.where({ start_station_id: @stations.uniq.pluck(:id)}).group('start_station_id').order('count_start_station_id DESC').count('start_station_id').first[0])
 
-    @geojson << {
+
+
+    # Setup GeoJSON array
+    @heatmapJSON = Array.new
+    @cityJSON = Array.new
+    @geoJSON = Array.new
+    @payloadJSON = Array.new
+
+    @intesity
+    # Push heat map data to paylod
+    @stations.each do |s|
+      @heatmapJSON << [s.lat,s.lon,s.start_trips.count+s.end_trips.count]
+    end
+
+    @payloadJSON << @heatmapJSON
+
+
+    # Push city stats to GeoJSON
+    @cityJSON << {
       city: params[:city],
       station_count: @stations.count,
+      bike_count: @stations.sum(:dockcount),
       popular_dropoff: popular_dropoff.name,
       popular_pickup: popular_pickup.name
     }
+
+    @payloadJSON << @cityJSON
+
 
     @stations.each do |s|
 
       most_popular_end_station = @stations.find(s.start_trips.group('end_station_id').order('count_end_station_id DESC').count('end_station_id').first[0])
       most_popular_start_station = @stations.find(s.end_trips.group('start_station_id').order('count_start_station_id DESC').count('start_station_id').first[0])
 
-      @geojson << {
+      @geoJSON << {
         type: 'Feature',
         geometry: {
           type: 'Point',
@@ -53,8 +77,10 @@ class StationsController < ApplicationController
       }
     end
 
+    @payloadJSON << @geoJSON
+
     respond_to do |format|
-      format.json { render json: @geojson}
+      format.json { render json: @payloadJSON}
     end
   end
 end
