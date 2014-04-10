fdcdc4d79714f6d549f86da87cd99e338398a56d
Roy McKenzie <roymckenzie@gmail.com>
2014-04-10 13:11:09 -0700

    Added a pause on CalTrain model creation to not timeout Google geocoder.

--- app/models/caltrain_station.rb
+++ app/models/caltrain_station.rb
@@ -4,7 +4,9 @@ reverse_geocoded_by :lat, :lon do |obj,results|
   if geo = results.first
     obj.city    = geo.city
   end
+  sleep(2) # Google Geocoder API may deny requests to geocode if they come in to fast.
 end
+
 after_validation :reverse_geocode
 
 end
